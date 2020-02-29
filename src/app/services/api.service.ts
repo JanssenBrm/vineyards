3ee43be334fce586757d717aeaf0957db03e75e1
janssenb <bram.janssen@vito.be>
2020-02-29 17:46:29 +0100

    Added behavioursubject

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,19 +2,30 @@ import { UtilService } from './util.service';
 import { Vineyard } from './../models/vineyard.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { switchMap, map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor( private http: HttpClient, private utilService: UtilService) { }
+  private _vineyards: BehaviorSubject<Vineyard[]>;
+
+  constructor( private http: HttpClient, private utilService: UtilService) {
+      this._vineyards = new BehaviorSubject<Vineyard[]>([]);
+      this.readVineyards();
+  }
 
   getVineyards(): Observable<Vineyard[]> {
-    return this.http.get('./assets/mock/vineyards.json').pipe(
-      map((data: Vineyard[]) => data.map((v: Vineyard) => this.utilService.reproject(v, 'EPSG:4326', 'EPSG:3857')))
-    );
+    return this._vineyards;
+  }
+
+  private readVineyards(): void {
+    this.http.get('./assets/mock/vineyards.json').pipe(
+      map((data: Vineyard[]) => data.map((v: Vineyard) => this.utilService.reproject(v, 'EPSG:4326', 'EPSG:3857'))),
+    ).subscribe((vineyards: Vineyard[]) => {
+      this._vineyards.next(vineyards);
+    });
   }
 }
