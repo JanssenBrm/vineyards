7ea60a40dd9157351ae17d0e6688c3882df4a558
janssenb <bram.janssen@vito.be>
2020-03-28 13:48:15 +0100

    Added bbch stages

---
+++ src/app/conf/bbch.config.ts
@@ -0,0 +1,160 @@
+import { BBCH } from '../models/bbch.model';
+
+export const BBCH_STAGES: BBCH[] = [
+  {
+    code: '00',
+    description:
+      'Dormancy: winter buds pointed to rounded, light or dark brown according to cultivar; bud scales more or less closed according to cultivar'
+  },
+  {
+    code: '01',
+    description:
+      'Beginning of bud swelling: buds begin to expand inside the bud scales'
+  },
+  {
+    code: '03',
+    description: 'End of bud swelling: buds swollen, but not green'
+  },
+  {
+    code: '05',
+    description: 'Wool stage: brown wool clearly visible'
+  },
+  {
+    code: '07',
+    description: 'Beginning of bud burst: green shoot tips just visible'
+  },
+  {
+    code: '09',
+    description: 'Bud burst: green shoot tips clearly visible'
+  },
+  {
+    code: '11',
+    description: 'First leaf unfolded and spread away from shoot'
+  },
+  {
+    code: '12',
+    description: '2nd leaves unfolded'
+  },
+  {
+    code: '15',
+    description: '5th leaves unfolded'
+  },
+  {
+    code: '19',
+    description: '9 or more leaves unfolded'
+  },
+  {
+    code: '53',
+    description: 'Inflorescences clearly visible'
+  },
+  {
+    code: '55',
+    description: 'Inflorescences swelling, flowers closely pressed together'
+  },
+  {
+    code: '57',
+    description: 'Inflorescences fully developed; flowers separating'
+  },
+  {
+    code: '60',
+    description: 'First flowerhoods detached from the receptacle'
+  },
+  {
+    code: '61',
+    description: 'Beginning of flowering: 10% of flowerhoods fallen'
+  },
+  {
+    code: '62',
+    description: '20% of flowerhoods fallen'
+  },
+  {
+    code: '63',
+    description: 'Early flowering: 30% of flowerhoods fallen'
+  },
+  {
+    code: '64',
+    description: '40% of flowerhoods fallen'
+  },
+  {
+    code: '65',
+    description: 'Full flowering: 50% of flowerhoods fallen'
+  },
+  {
+    code: '66',
+    description: '60% of flowerhoods fallen'
+  },
+  {
+    code: '67',
+    description: '70% of flowerhoods fallen'
+  },
+  {
+    code: '68',
+    description: '80% of flowerhoods fallen'
+  },
+  {
+    code: '69',
+    description: 'End of flowering'
+  },
+  {
+    code: '71',
+    description:
+      'Fruit set: young fruits begin to swell, remains of flowers lost'
+  },
+  {
+    code: '73',
+    description: 'Berries groat-sized, bunches begin to hang'
+  },
+  {
+    code: '75',
+    description: 'Berries pea-sized, bunches hang'
+  },
+  {
+    code: '77',
+    description: 'Berries beginning to touch'
+  },
+  {
+    code: '79',
+    description: 'Majority of berries touching'
+  },
+  {
+    code: '81',
+    description:
+      'Beginning of ripening: berries begin to develop variety-specific colour'
+  },
+  {
+    code: '83',
+    description: 'Berries developing colour'
+  },
+  {
+    code: '85',
+    description: 'Softening of berries'
+  },
+  {
+    code: '89',
+    description: 'Berries ripe for harvest'
+  },
+  {
+    code: '91',
+    description: 'After harvest; end of wood maturation'
+  },
+  {
+    code: '92',
+    description: 'Beginning of leaf discolouration'
+  },
+  {
+    code: '93',
+    description: 'Beginning of leaf-fall'
+  },
+  {
+    code: '95',
+    description: '50% of leaves fallen'
+  },
+  {
+    code: '97',
+    description: 'End of leaf-fall'
+  },
+  {
+    code: '99',
+    description: 'Harvested product'
+  }
+];
