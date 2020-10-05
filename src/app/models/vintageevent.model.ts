5b754078894072857b2a95370a650a738715d15f
janssenb <bram.janssen@vito.be>
2020-10-05 08:04:46 +0200

    Updated of vintage events

--- src/app/models/vintageevent.model.ts
+++ src/app/models/vintageevent.model.ts
@@ -3,9 +3,10 @@ export enum VintageEvent {
    'FIRST_FERMENTATION' = 'First Fermentation',
    'PRESSING' = 'Pressed',
    'RACKING' = 'Racked',
-   'RESTING' = 'Resting',
-   'BOTTLING' = 'Bottling',
-   'TASTING' = 'Tasting'
+   'RESTING' = 'Rest',
+   'TESTING' = 'Test',
+   'BOTTLING' = 'Bottled',
+   'TASTING' = 'Tasted'
 }
 
 export const SINGLE_DATES = [VintageEvent.PRESSING, VintageEvent.RACKING, VintageEvent.BOTTLING, VintageEvent.TASTING];
