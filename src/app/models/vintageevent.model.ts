06e0c80143a79dffc1db686092db72939114b208
janssenb <bram.janssen@vito.be>
2021-05-15 09:12:16 +0200

    Fixed issue where tasted was not colored in graph

--- src/app/models/vintageevent.model.ts
+++ src/app/models/vintageevent.model.ts
@@ -1,23 +1,23 @@
-
 export enum VintageEvent {
-   'FIRST_FERMENTATION' = 'First Fermentation',
-   'PRESSING' = 'Pressed',
-   'RACKING' = 'Racked',
-   'RESTING' = 'Rest',
-   'TESTING' = 'Test',
-   'BOTTLING' = 'Bottled',
-   'TASTING' = 'Tasted'
+    'FIRST_FERMENTATION' = 'First Fermentation',
+    'PRESSING' = 'Pressed',
+    'RACKING' = 'Racked',
+    'RESTING' = 'Rest',
+    'TESTING' = 'Test',
+    'BOTTLING' = 'Bottled',
+    'TASTING' = 'Tasted'
 }
 
 export const SINGLE_DATES = [VintageEvent.PRESSING, VintageEvent.RACKING, VintageEvent.BOTTLING, VintageEvent.TASTING, VintageEvent.TESTING];
 
 
 export const VINTAGEEVENT_COLORS =
-   [
-      'rgb(255, 205, 86)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 99, 132)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)'
-   ];
+    [
+        'rgb(255, 205, 86)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(56,128,255)'
+    ];
