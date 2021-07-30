ef929694c9f21b4fda08d4d0440bfaf738cbfdf5
janssenb <bram.janssen@vito.be>
2021-07-30 11:03:03 +0200

    Updated auth domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   api: 'http://localhost:5000/winery-f4d20/us-central1/',
   firebase: {
     apiKey: "AIzaSyBUuVCnRdswfIv_AOJgbfRbo_R7th0naQo",
-    authDomain: "winery-f4d20.firebaseapp.com",
+    authDomain: "myvineyards.tk",
     databaseURL: "https://winery-f4d20.firebaseio.com",
     projectId: "winery-f4d20",
     storageBucket: "winery-f4d20.appspot.com",
