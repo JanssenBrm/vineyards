1995eb933fa835f80336e6e8046bbb35701fa3dd
Bram Janssen <brm.janssen@gmail.com>
2022-07-15 16:22:13 +0200

    refactor: generate environments file from env

---
+++ setenv.ts
@@ -0,0 +1,37 @@
+const {writeFile} = require('fs');
+const {argv} = require('yargs');
+
+require('dotenv').config();
+
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+const targetPath = isProduction
+    ? `./src/environments/environment.prod.ts`
+    : `./src/environments/environment.ts`;
+
+const environmentFileContent = `
+export const environment = {
+      production: ${isProduction},
+      api: ${process.env.API_URL},
+      firebase: {
+        apiKey: ${process.env.FB_API_KEY},
+        authDomain: ${process.env.FB_AUTH_DOMAIN},
+        databaseURL: ${process.env.FB_DATABASE_URL},
+        projectId: ${process.env.FB_PROJECT_ID},
+        storageBucket: ${process.env.FB_STORAGE_BUCKET},
+        messagingSenderId: ${process.env.FB_MESSAGE_SENDER_ID},
+        appId: ${process.env.FB_APP_ID},
+        measurementId: ${process.env.FB_MEASUREMENT_ID}
+  },
+  owm_key: ${process.env.OWM_KEY}
+};
+`;
+
+writeFile(targetPath, environmentFileContent, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`Wrote variables to ${targetPath}`);
+});
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,6 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:5000/winery-f4d20/us-central1/',
-  firebase: {
-    apiKey: "AIzaSyBUuVCnRdswfIv_AOJgbfRbo_R7th0naQo",
-    authDomain: "myvineyards.tk",
-    databaseURL: "https://winery-f4d20.firebaseio.com",
-    projectId: "winery-f4d20",
-    storageBucket: "winery-f4d20.appspot.com",
-    messagingSenderId: "863507070004",
-    appId: "1:863507070004:web:c53b2af1bcb19b44779e61",
-    measurementId: "G-L6YC96115N"
-  },
-  owm_key: '4c72c6cc5bdf22958b2ad9088eb46c10'
 };
 
 /*
