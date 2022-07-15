009641f5cddfd05d7e9d162ab541958d1426433d
Bram Janssen <brm.janssen@gmail.com>
2022-07-15 16:41:12 +0200

    build: updated creating env file

---
+++ scripts/create_env_file.sh
@@ -0,0 +1,11 @@
+file='.env'
+echo "FB_API_KEY=$FB_API_KEY" > $file
+echo "FB_AUTH_DOMAIN=$FB_AUTH_DOMAIN" >> $file
+echo "FB_DATABASE_URL=$FB_DATABASE_URL" >> $file
+echo "FB_PROJECT_ID=$FB_PROJECT_ID" >> $file
+echo "FB_STORAGE_BUCKET=$FB_STORAGE_BUCKET" >> $file
+echo "FB_MESSAGE_SENDER_ID=$FB_MESSAGE_SENDER_ID" >> $file
+echo "FB_APP_ID=$FB_APP_ID" >> $file
+echo "FB_MEASUREMENT_ID=$FB_MEASUREMENT_ID" >> $file
+echo "OWM_KEY=$OWM_KEY" >> $file
+echo "API_URL=$API_URL" >> $file
