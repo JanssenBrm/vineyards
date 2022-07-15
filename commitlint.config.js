4fa5aba335d4a5a0eb9d8fcd9811955daf394603
Bram Janssen <brm.janssen@gmail.com>
2022-07-15 17:14:53 +0200

    fix: updated commitlint to cope with semantic-release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-leading-blank': [1, 'always'],
+        'body-max-line-length': [2, 'always', Infinity],
+    }
+}
