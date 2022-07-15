bcee80ac1ddf2d4ee240bb6d272acb739172edbe
Bram Janssen <brm.janssen@gmail.com>
2022-07-15 16:28:00 +0200

    refactor: added config build to start and build commands

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -12,19 +12,19 @@ const targetPath = isProduction
 
 const environmentFileContent = `
 export const environment = {
-      production: ${isProduction},
-      api: ${process.env.API_URL},
+      production: "${isProduction}",
+      api: "${process.env.API_URL}",
       firebase: {
-        apiKey: ${process.env.FB_API_KEY},
-        authDomain: ${process.env.FB_AUTH_DOMAIN},
-        databaseURL: ${process.env.FB_DATABASE_URL},
-        projectId: ${process.env.FB_PROJECT_ID},
-        storageBucket: ${process.env.FB_STORAGE_BUCKET},
-        messagingSenderId: ${process.env.FB_MESSAGE_SENDER_ID},
-        appId: ${process.env.FB_APP_ID},
-        measurementId: ${process.env.FB_MEASUREMENT_ID}
+        apiKey: "${process.env.FB_API_KEY}",
+        authDomain: "${process.env.FB_AUTH_DOMAIN}",
+        databaseURL: "${process.env.FB_DATABASE_URL}",
+        projectId: "${process.env.FB_PROJECT_ID}",
+        storageBucket: "${process.env.FB_STORAGE_BUCKET}",
+        messagingSenderId: "${process.env.FB_MESSAGE_SENDER_ID}",
+        appId: "${process.env.FB_APP_ID}",
+        measurementId: "${process.env.FB_MEASUREMENT_ID}"
   },
-  owm_key: ${process.env.OWM_KEY}
+  owm_key: "${process.env.OWM_KEY}"
 };
 `;
 
