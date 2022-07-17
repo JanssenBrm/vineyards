7f4cf0a2e8a757ccc60a1e92539f25bcc9f77995
janssenb <bram.janssen@vito.be>
2022-07-17 15:11:50 +0200

    refactor: only enable sentry on live app

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from "@sentry/angular";
-import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from '@sentry/angular';
+import { BrowserTracing } from '@sentry/tracing';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-
 Sentry.init({
-  dsn: "https://094234f9e1024595801515548fd3d9cc@o1319586.ingest.sentry.io/6575225",
+  dsn: 'https://094234f9e1024595801515548fd3d9cc@o1319586.ingest.sentry.io/6575225',
   integrations: [
     new BrowserTracing({
-      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      tracingOrigins: ['http://myvineyards.tk/'],
       routingInstrumentation: Sentry.routingInstrumentation,
     }),
   ],
@@ -19,11 +18,13 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+  enabled: !!environment.production,
 });
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
