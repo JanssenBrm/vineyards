35e1da130fd094fe2ce1be7f65cd356802520931
janssenb <bram.janssen@vito.be>
2022-07-19 18:21:57 +0200

    feat: integrated cache in weather station data

--- src/app/services/weatherstation.service.ts
+++ src/app/services/weatherstation.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Integration } from '../models/integration.model';
 import { HttpClient } from '@angular/common/http';
 import { WeatherStationInfo } from '../models/weather.model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherStationService {
+  private dataCache: { key: string; data: WeatherStationInfo[] }[] = [];
+
   constructor(private http: HttpClient) {}
 
   public readWeatherData(integration: Integration): Observable<WeatherStationInfo[]> {
-    return this.http.get<WeatherStationInfo[]>(`${integration.url}/daily?apiKey=${integration.key}`);
+    const hit = this.dataCache.find((c) => c.key === integration.key);
+    if (hit) {
+      return of(hit.data);
+    } else {
+      return this.http.get<WeatherStationInfo[]>(`${integration.url}/daily?apiKey=${integration.key}`).pipe(
+        tap((data: WeatherStationInfo[]) => {
+          this.dataCache.push({
+            key: integration.key,
+            data,
+          });
+        })
+      );
+    }
   }
 }
