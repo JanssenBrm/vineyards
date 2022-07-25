21f8a17e54270a8ec7a4b88aeb1c9c1339654951
janssenb <bram.janssen@vito.be>
2022-07-25 18:12:02 +0200

    fix: removed empty css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: [],
 })
 export class AppComponent {
   constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
