e7b35b27546aa60db0986d344c0d0714dba0306d
bramjanssen <bram.janssen@vito.be>
2023-10-31 19:37:34 +0100

    feat: added full config for sharing fields

--- functions/src/services/vineyards.service.ts
+++ functions/src/services/vineyards.service.ts
@@ -24,6 +24,19 @@ const addPermissions = async (
     throw error;
   }
 };
+
+const addSharedVineyard = async (userId: string, ownerId: string, vineyardId: string) => {
+  try {
+    console.log(`Adding vineyard ${vineyardId} of ${ownerId} to shared vineyards of ${userId}`);
+    await db.collection('users').doc(userId).collection('sharedVineyards').doc(vineyardId).set({
+      user: ownerId,
+      vineyard: vineyardId,
+    });
+  } catch (error) {
+    console.error(`Could not add ${vineyardId} of ${ownerId} to shared vineyards of ${userId}`, error);
+    throw error;
+  }
+};
 const deletePermissions = async (ownerId: string, vineyardId: string, user: string): Promise<void> => {
   try {
     console.log(`Removing permissions from vineyard ${vineyardId} of ${ownerId} for user ${user}`);
@@ -40,9 +53,20 @@ const deletePermissions = async (ownerId: string, vineyardId: string, user: stri
     throw error;
   }
 };
+const deleteSharedVineyard = async (userId: string, ownerId: string, vineyardId: string) => {
+  try {
+    console.log(`Deleting vineyard ${vineyardId} of ${ownerId} to shared vineyards of ${userId}`);
+    await db.collection('users').doc(userId).collection('sharedVineyards').doc(vineyardId).delete();
+  } catch (error) {
+    console.error(`Could not delete ${vineyardId} of ${ownerId} to shared vineyards of ${userId}`, error);
+    throw error;
+  }
+};
 export const shareVineyard = async (ownerId: string, vineyardId: string, opts: SharingOpts): Promise<void> => {
   await addPermissions(ownerId, vineyardId, opts.user, opts.permissions);
+  await addSharedVineyard(opts.user, ownerId, vineyardId);
 };
 export const unshareVineyard = async (ownerId: string, vineyardId: string, userId: string): Promise<void> => {
   await deletePermissions(ownerId, vineyardId, userId);
+  await deleteSharedVineyard(userId, ownerId, vineyardId);
 };
